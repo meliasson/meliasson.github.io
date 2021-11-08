46f6e73a977fbef8882ebb08fa30e464f0edf231
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2021-11-08 20:33:08 +0100

    Move player model to root model

--- assets/game.js
+++ assets/game.js
@@ -3,32 +3,35 @@
     nrOfColumns: 80,
     nrOfRows: 45,
     grid: [],
-    players: [],
+    player: null,
     init: function () {
+      // Create grid.
       for (let row = 0; row < this.nrOfRows; row++) {
         this.grid[row] = [];
         for (let col = 0; col < this.nrOfColumns; col++) {
           this.grid[row][col] = null;
         }
       }
+
+      // Create player in middle of grid.
+      const headX = Math.floor(this.nrOfColumns / 2);
+      const headY = Math.floor(this.nrOfRows / 2);
+      this.player = {
+        direction: "east",
+        body: [
+          { x: headX, y: headY },
+          { x: headX + 1, y: headY },
+          { x: headX + 2, y: headY },
+          { x: headX + 3, y: headY },
+        ],
+      };
     },
     step: function () {
       console.log("let's take a step", this.grid);
-      //
     },
   };
 
   model.init();
-
-  const modelPlayer = {
-    direction: "east",
-    body: [
-      { x: 39, y: 9 },
-      { x: 38, y: 9 },
-      { x: 37, y: 9 },
-      { x: 36, y: 9 },
-    ],
-  };
 
   //
   // Controller stuff below.
@@ -39,13 +42,13 @@
       event.preventDefault();
     }
     if (event.keyCode === 37) {
-      modelPlayer.direction = "west";
+      model.player.direction = "west";
     } else if (event.keyCode === 38) {
-      modelPlayer.direction = "north";
+      model.player.direction = "north";
     } else if (event.keyCode === 39) {
-      modelPlayer.direction = "east";
+      model.player.direction = "east";
     } else if (event.keyCode === 40) {
-      modelPlayer.direction = "south";
+      model.player.direction = "south";
     }
   });
 
@@ -53,18 +56,18 @@
   // go to e.g. lobby?
   window.setInterval(() => {
     // Model: Update player model.
-    const head = Object.assign({}, modelPlayer.body[0]);
-    modelPlayer.body.unshift(head);
-    if (modelPlayer.direction === "west") {
-      modelPlayer.body[0].x -= 1;
-    } else if (modelPlayer.direction === "north") {
-      modelPlayer.body[0].y -= 1;
-    } else if (modelPlayer.direction === "east") {
-      modelPlayer.body[0].x += 1;
-    } else if (modelPlayer.direction === "south") {
-      modelPlayer.body[0].y += 1;
+    const head = Object.assign({}, model.player.body[0]);
+    model.player.body.unshift(head);
+    if (model.player.direction === "west") {
+      model.player.body[0].x -= 1;
+    } else if (model.player.direction === "north") {
+      model.player.body[0].y -= 1;
+    } else if (model.player.direction === "east") {
+      model.player.body[0].x += 1;
+    } else if (model.player.direction === "south") {
+      model.player.body[0].y += 1;
     }
-    modelPlayer.body.pop();
+    model.player.body.pop();
 
     // Model: Clear grid.
     for (let i = 0; i < model.grid.length; i += 1) {
@@ -74,8 +77,8 @@
     }
 
     // Model: Place player in grid. TODO: Check for collisions.
-    for (let i = 0; i < modelPlayer.body.length; i += 1) {
-      bodyPart = modelPlayer.body[i];
+    for (let i = 0; i < model.player.body.length; i += 1) {
+      bodyPart = model.player.body[i];
       model.grid[bodyPart.y][bodyPart.x] = true;
     }
 
